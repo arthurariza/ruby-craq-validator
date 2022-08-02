18f91fc0fae7d37355e8f94f416f2bf6d51f00db
arthurariza <tucaariza@gmail.com>
2022-08-02 01:28:44 -0300

    tests: terminal questions

--- spec/craq_validator_spec.rb
+++ spec/craq_validator_spec.rb
@@ -34,14 +34,48 @@ describe CraqValidator do
     ]
   end
 
+  let(:terminal_questions) do
+    [
+      {
+        text: 'question one?',
+        options: [{ text: '1' }, { text: '2' }]
+      },
+      {
+        text: 'question two?',
+        options: [{ text: '1' }, { text: '2', complete_if_selected: true }]
+      },
+      {
+        text: 'question three?',
+        options: [{ text: '1' }, { text: '2' }]
+      }
+    ]
+  end
+
   let(:answers_correct) { { q0: 1, q1: 0 } }
   let(:answers_invalid_list) { { q0: 3, q1: 0 } }
   let(:answers_not_answered) { { q1: 0, q2: 0 } }
+  let(:terminal_answers_not_answered) { { q0: 0, q1: 0, q2: 1 } }
+  let(:terminal_answers_is_answered) { { q0: 0, q1: 1 } }
+  let(:terminal_answers_shouldnt_be_answered) { { q0: 0, q1: 1, q2: 1 } }
 
   describe '#validate' do
     context 'returns valid' do
       it 'when all questions are answered' do
         validator = CraqValidator.new(questions, answers_correct)
+
+        expect(validator.validate).to be_truthy
+        expect(validator.errors).to be_empty
+      end
+
+      it 'when terminal question is not answered' do
+        validator = CraqValidator.new(terminal_questions, terminal_answers_not_answered)
+
+        expect(validator.validate).to be_truthy
+        expect(validator.errors).to be_empty
+      end
+
+      it 'when terminal question is answered' do
+        validator = CraqValidator.new(terminal_questions, terminal_answers_is_answered)
 
         expect(validator.validate).to be_truthy
         expect(validator.errors).to be_empty
@@ -71,6 +105,14 @@ describe CraqValidator do
         expect(validator.validate).to be_falsey
         expect(validator.errors).not_to be_empty
         expect(validator.errors).to include(q0: 'was not answered')
+      end
+
+      it 'when terminal question is answered' do
+        validator = CraqValidator.new(terminal_questions, terminal_answers_shouldnt_be_answered)
+
+        expect(validator.validate).to be_falsey
+        expect(validator.errors).not_to be_empty
+        expect(validator.errors).to include(q2: 'was answered even though a previous response indicated that the questions were complete')
       end
     end
   end
